1252bf3946004d7d2d5f85cd030cd9e150433cf5
Armando <11156050+ArmandoShala@users.noreply.github.com>
2022-11-30 09:11:18 +0100

    P10 finished

--- MOBA1_assignment_10/script.js
+++ MOBA1_assignment_10/script.js
@@ -1,11 +1,11 @@
 toggleClass = () => {
-    var element = document.getElementById("imgWrapper");
-    if (element.classList.contains("big")) {
-        element.classList.remove("big");
-        element.classList.add("small");
-    } else {
+    var element = document.getElementById("exampleImage");
+    if(document.getElementById("togBtn").checked === true){
         element.classList.remove("small");
         element.classList.add("big");
+
+    } else {
+        element.classList.remove("big");
+        element.classList.add("small");
     }
-    document.getElementById("currClass").innerHTML = element.className;
 }
