c674b119d93386049f41b41046899bec1590e60a
Armando <dreiteilig.backoefen_0f@icloud.com>
2022-12-12 23:37:47 +0100

    P12 started

--- MOBA1_assignment_12/js/main.js
+++ MOBA1_assignment_12/js/main.js
@@ -1,34 +1,7 @@
-// window.addEventListener("deviceorientation", handleOrientation, true);
-
-// function handleOrientation(event) {
-//   const absolute = event.absolute;
-//   const alpha = event.alpha; // explain: https://developer.mozilla.org/en-US/docs/Web/API/Detecting_device_orientation
-//   const beta = event.beta;
-//   const gamma = event.gamma;
-// }
-
-//
-// const gyroscope = new Gyroscope({frequency: 60});
-// gyroscope.addEventListener('reading', (e) => {
-//   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-//   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-//   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-// });
-// gyroscope.start();
-
-
-
-const gyroscope = new Gyroscope();
-
-gyroscope.start();
-gyroscope.addEventListener('reading', e => {
-  console.log("X-axis: " + gyroscope.x);
-  console.log("Y-axis: " + gyroscope.y);
-  console.log("Z-axis: " + gyroscope.z);
-
-  // rotate arrow arrow1 according to gyroscope
-  document.getElementById("arrow1").style.transform = "rotate(" + gyroscope.x + "deg)";
-
+window.addEventListener('deviceorientation', function (event) {
+    document.getElementById('arrow1').style.webkitTransform
+  = document.getElementById('arrow1').style.transform =
+        'rotateX(' + event.beta + 'deg) ' +
+        'rotateY(' + event.gamma + 'deg) ' +
+        'rotateZ(' + event.alpha + 'deg)';
 });
-
-
